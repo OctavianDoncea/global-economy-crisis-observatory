1ef76befa87583a8d7317bf926a954cd9d0fb56f
Octavian Doncea <octaviandoncea@gmail.com>
2026-04-27 18:18:35 +0300

    Script for creating multiple PostgreSQL databases

---
+++ docker/postgres/init-multiple-dbs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -u
+
+if [ -n "${POSTGRES_MULTIPLE_DATABASES:-}" ]; then
+    echo "Multiple-database init: $POSTGRES_MULTIPLE_DATABASES"
+    for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
+        echo "Creating database: $db"
+        psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+            SELECT 'CREATE DATABASE $db'
+            WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$db')\gexec
+EOSQL
+    done
+    echo "Multi-database init complete."
+fi
